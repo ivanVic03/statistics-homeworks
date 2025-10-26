f8ac25552b3aa6bb1c26eef2b0ca98ac78be535b
ivanVic03 <viceconti.2043692@studenti.uniroma1.it>
2025-10-26 12:52:44 +0100

    Implementing homework4 JavaScript

--- homeworks/homework_4/src/homework4.js
+++ homeworks/homework_4/src/homework4.js
@@ -2,3 +2,50 @@ let p, m, n;
 let trajectories = [];
 let currentIndex = 0;
 
+function simulateTrial(p) {
+    if (Math.random() < p) {
+        return 1;
+    }
+    else return 0;
+}
+
+function simulateTrajectory(p, n) {
+    let successes = 0;
+    let frequencies = [];
+    for (let i = 0; i < n; i++) {
+        successes += simulateTrial(p);
+        frequencies.push(successes / i);
+    }
+    return frequencies;
+}
+
+function generateTrajectories(p,n,m) {
+    let trajectories = [];
+    for (let i = 0; i < n; i++) {
+        trajectories.push(simulateTrajectory(p,n));
+    }
+    return trajectories;
+}
+
+function startSimulation() {
+    p = parseFloat(document.getElementById("p").value);
+    m = parseFloat(document.getElementById("m").value);
+    n = parseFloat(document.getElementById("n").value);
+
+    trajectories = generateTrajectories(p, n, m);
+    currentIndex = 0;
+
+    const ctxMain = document.getElementById("mainChart").getContext("2d");
+    if (window.mainChart) {
+        window.mainChart.destroy()
+    }
+    window.mainChart = new Chart(ctxMain, {
+        type: 'line',
+        data: {
+            labels: Array.from({ length: n }, (_, i) => i+1),
+            datasets: [{
+                label:
+            }]
+        }
+    })
+}
